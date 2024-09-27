3094c746156772fddec60fc6254c3573974b50cd
Tural Musaev <163137546+Fandom123023@users.noreply.github.com>
2024-09-27 16:04:49 +0500

    swager1.00

--- school/src/main/java/ru/hogwarts/school/model/Student.java
+++ school/src/main/java/ru/hogwarts/school/model/Student.java
@@ -51,7 +51,7 @@ public class Student {
     }
 
     @Override
-    public String toString() {
+    public String  toString() {
         return "Student{" +
                 "id=" + id +
                 ", name='" + name + '\'' +
